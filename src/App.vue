c03bd97a7f23bbb92fe44880c498745e997474e2
rchaser53 <tayoshizawa29@gmail.com>
2018-03-31 15:47:41 +0900

    use slot

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div style="display: flex;">
+		<slot></slot>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class extends Vue {
+	parentValue = 'parent value!!!'
+}
+</script>
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import router from './router'
 
+import App from './App.vue'
+
 new Vue({
 	el: '#app',
-	router,
-	template: '<router-view />'
+	router, 
+	components: {
+		App
+	}
 });
