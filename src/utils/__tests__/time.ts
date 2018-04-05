3937d647631e252f1d94b2bf21a9e46659d32523
rchaser53 <tayoshizawa29@gmail.com>
2018-04-05 20:20:17 +0900

    try to typed for moment

---
+++ src/utils/__tests__/time.ts
@@ -0,0 +1,17 @@
+import {
+	getFormattedDatetime,
+	isAfterDatetime
+} from '../time'
+
+describe('getFormattedDatetime', () => {
+	test('JP_SHORT', async () => {
+		const input = 1523000000000
+		expect(getFormattedDatetime(1523000000000, 'JP_SHORT')).toEqual('2018/04/06')
+	});
+	
+	test('Default', async () => {
+		const input = 1523000000000
+		expect(getFormattedDatetime(1523000000000)).toEqual('2018-04-06T16:33:20+09:00')
+	})
+})
+
