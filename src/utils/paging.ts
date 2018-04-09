fff3649c96844ec184d412dddab51b69cc059cb2
rchaser53 <tayoshizawa29@gmail.com>
2018-04-09 23:40:22 +0900

    create pages array

---
+++ src/utils/paging.ts
@@ -0,0 +1,60 @@
+export default class Pages {
+  limit: number = 50;
+  countOfPage: number = 4;
+
+  constructor(limit: number, countOfPage: number) {
+    this.limit = limit || this.limit
+    this.countOfPage = countOfPage || this.countOfPage
+  }
+
+  create(currentPage: number, totalRecord: number): number[] {
+    if (totalRecord < this.limit) return []
+    if (this.isCreatePagesFromStart(currentPage, totalRecord)) return this.createFromZero(totalRecord)
+  
+    return this.createPages(currentPage, totalRecord)
+  }
+
+  createOffset(): number {
+    return (this.countOfPage % 2 === 0)
+            ? Math.floor(this.countOfPage / 2)  - 1
+            : Math.floor(this.countOfPage / 2)
+  }
+
+  isCreatePagesFromStart(currentPage: number, totalRecord: number): boolean {
+    const offset = this.createOffset()
+    if (totalRecord < (this.limit * this.countOfPage)) return true
+    if (currentPage < offset) return true
+
+    return false
+  }
+
+  createFromZero(totalRecord: number): number[] {
+    const pages: number[] = []
+    const pagesLength =  Math.ceil(totalRecord / this.limit)
+    for (let i = 0; i < pagesLength; i++) {
+      pages.push(i)
+    }
+    return pages
+  }
+
+  createPages(currentPage: number, totalRecord: number): number[] {
+    const pages: number[] = []
+    const offset = this.createOffset()
+    const limitPageNumber = this.getLimitPageNumber(currentPage, totalRecord)
+
+    for (let i = currentPage - offset; i < limitPageNumber; i++) {
+      pages.push(i)
+    }
+    return pages
+  }
+
+  getLimitPageNumber(currentPage: number, totalRecord: number): number {
+    const pagesLength = Math.ceil(totalRecord / this.limit)
+    const offset = this.createOffset()
+    const fromStart = currentPage - offset + this.countOfPage
+
+    return (pagesLength < fromStart)
+              ? pagesLength
+              : fromStart
+  }
+}
