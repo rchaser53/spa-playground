6252557fd425e708ad20321f70056a56c6e2fee0
rchaser53 <tayoshizawa29@gmail.com>
2018-04-19 00:05:19 +0900

    rx zip with cancel

--- memo/rxjsZip.js
+++ memo/rxjsZip.js
@@ -6,17 +6,25 @@ const axiosRetry = require('axios-retry');
 const Axios = require('axios');
 
 const CancelToken = Axios.CancelToken;
+const source = CancelToken.source()
 const client = Axios.create({
   baseURL: 'http://localhost:3000/',
 })
 
-const obsA = Observable.fromPromise(client.get('test'))
-const obsB = Observable.fromPromise(client.get('test'))
-const obsC = Observable.fromPromise(client.get('test'))
+const obsA = Observable.fromPromise(client.get('test', { cancelToken: source.token }))
+const obsB = Observable.fromPromise(client.get('test', { cancelToken: source.token }))
+const obsC = Observable.fromPromise(client.get('test', { cancelToken: source.token }))
+
+source.cancel('nya-n')
 
 Observable.zip(obsA, obsB, obsC)
-          .subscribe((ret) => {
-            ret.forEach((ret) => {
-              console.log(ret.data)
-            })
-          })
+          .subscribe(
+            (ret) => {
+              ret.forEach((ret) => {
+                console.log(ret.data)
+              })
+            },
+            (err) => {
+              console.error(err)
+            }
+          )
