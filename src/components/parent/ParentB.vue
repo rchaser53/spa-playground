3d49644ed9d12f34739b3f967da417b2c71e216e
rchaser53 <tayoshizawa29@gmail.com>
2018-04-20 22:27:56 +0900

    extract class for type

--- src/components/parent/ParentB.vue
+++ src/components/parent/ParentB.vue
@@ -4,12 +4,36 @@ import Component, { mixins } from 'vue-class-component'
 
 import { httpClient, cancelToken } from '../../utils/http'
 import ParentTemplateMixin from './ParentTemplateMixinB.vue'
-import { createClass } from './lib/base.vue'
+import Parent, { createClass } from './lib/base.vue'
 
 const httpObject = {
-  client: httpClient,
-  cancelToken
+  client: httpClient
 }
-export default createClass(httpObject, ParentTemplateMixin)
+
+const decoratorObject = {
+  mixins: [ ParentTemplateMixin ],
+  mounted: async function(this: Parent) {
+    try {
+      const source = cancelToken.source()
+      const { data } = await httpClient.get(this.articleEndpoint, {
+        cancelToken: source.token
+      })
+      this.article = data;
+    } catch (err) {
+      console.error(err)
+    }
+  },
+  data() {
+    return {
+      articleEndpoint: '/article',
+      cancelToken: cancelToken.source(),
+    }
+  },
+  beforeDestroy(this: Parent) {
+    this.cancelToken.cancel('move another path')
+  },
+}
+
+export default createClass(httpObject, decoratorObject)
 
 </script>
--- src/components/parent/lib/base.vue
+++ src/components/parent/lib/base.vue
@@ -5,53 +5,37 @@ import Component, { mixins } from 'vue-class-component'
 import ParentTemplateMixin from './ParentTemplateMixin.vue'
 import Child from '../../Child.vue'
 
-export const createClass = function(httpObj, templateMixin, overwriteDecorator = {}) {
-  const { client, cancelToken } = httpObj;
+@Component
+export class Parent extends Vue {
+  article = {
+    textA: '',
+    textB: '',
+    selectA: ''
+  }
+  articleEndpoint: string = ''
+  cancelToken: any
+
+  createSetter(key: string) {
+    return (value) => {
+      Vue.set(this.article, key, value)
+    }
+  }
+}
+
+export const createClass = function(httpObj, overwriteDecorator = {}) {
   const decoratorObj = {
     components: {
       Child,
-    },
-    mounted: async function(this: Base) {
-      try {
-        const { data } = await client.get(this.articleEndpoint, {
-          cancelToken: this.cancelToken.token
-        })
-        this.article = data;
-      } catch (err) {
-        console.error(err)
-      }
-    },
-    data() {
-      return {
-        articleEndpoint: '/article'
-      }
-    },
-    beforeDestroy(this: Base) {
-      this.cancelToken.cancel('move another path')
     },
     ...overwriteDecorator
     // data, components, methodなどを変更する予定
   }
 
   @Component(decoratorObj)
-  class Base extends mixins(templateMixin) {
-    cancelToken = cancelToken.source()
-    article = {
-      textA: '',
-      textB: '',
-      selectA: ''
-    }
-    articleEndpoint: string = ''
-
-    createSetter(key: string) {
-      return (value) => {
-        Vue.set(this.article, key, value)
-      }
-    }
-  }
+  class Base extends Parent {}
 
   return Base
 }
 
-export default {}
+export default Parent
 </script>
