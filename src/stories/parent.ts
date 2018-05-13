e060d3a0b0893b73bd76ff03916a0c489bf79a5b
rchaser53 <tayoshizawa29@gmail.com>
2018-05-13 16:24:58 +0900

    change to insert simple mock insteadof axios mock

--- src/stories/parent.ts
+++ src/stories/parent.ts
@@ -1,43 +1,27 @@
-import Axios from 'axios'
-export const axios = Axios.create()
-
 import { createClass } from '../components/parent/parent/Parent.vue'
 import { insertUtilMixins as insertToParentA } from '../components/parent/ParentA.vue'
 import { insertUtilMixins as insertToParentB } from '../components/parent/ParentB.vue'
 import { insertMockHttpClient } from './utils/mockHttpClient.vue'
 
-const mockHttpClient = Axios.create()
-mockHttpClient.interceptors.request.use(
-  request => null,
-  request => null
-)
-
-mockHttpClient.interceptors.response.use((response) => {
-  return Promise.resolve({
-    status: 200,
-    data: {
-      textA: '',
-      textB: '',
-      selectA: ''
+const createNomalMixin = () => {
+  const mockClient = {
+    get() {
+      return Promise.resolve({
+        data: {
+          textA: 'textA',
+          textB: 'textB',
+          selectA: ''
+        }
+      })
     }
-  })
-}, (err) => {
-  return Promise.resolve({
-    status: 200,
-    data: {
-      textA: 'textA',
-      textB: 'textB',
-      selectA: ''
-    }
-  })
-})
+  }
+  return insertMockHttpClient(mockClient)
+}
 
-const MixinClass = insertMockHttpClient(mockHttpClient)
-
+const NormalMixinClass = createNomalMixin()
 export const ParentA = createClass(
-  insertToParentA([ MixinClass ])
+  insertToParentA([ NormalMixinClass ])
 )
-
 export const ParentB = createClass(
-  insertToParentB([ MixinClass ])
+  insertToParentB([ NormalMixinClass ])
 )
